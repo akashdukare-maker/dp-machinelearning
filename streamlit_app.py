a4e6a2b42742b0b82c337ea0e35ec4e12f1a8cbc
akashdukare-maker <dukareakash199@gmail.com>
2025-11-01 20:34:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,4 +19,4 @@ y=df.species
 y
 
 with st.expander('Data visualization'):
-   st.scatter_chart(data=df,x='bill_length_mm',y='body_mass_g',colour='species')
+   st.scatter_chart(data=df,x='bill_length_mm', y='body_mass_g', colour='species')
