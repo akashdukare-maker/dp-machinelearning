54b925d9832e72b62692f3a7dd594db3c0062803
akashdukare-maker <dukareakash199@gmail.com>
2025-11-01 20:21:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ st.title('🤖 Machine Learning App')
 
 st.info('This is Apps Builds a Mchine Learning Model')
 
-
-df=pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
-df
+with st.expander('Data'):
+   st.write('**Raw data**')
+   df=pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+   df
